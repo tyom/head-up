cada7255e19500240953c28cb38f6a304503ca46
Tyom Semonov <inspiro@gmail.com>
2018-11-29 00:02:45 +0000

    Add initial tests

---
+++ packages/head-up/src/components/Cell/CellSettings.test.js
@@ -0,0 +1,7 @@
+import { shallowMount } from '@vue/test-utils';
+import CellSettings from './CellSettings';
+
+test('default render', () => {
+  const wrapper = shallowMount(CellSettings);
+  expect(wrapper).toMatchSnapshot();
+});
