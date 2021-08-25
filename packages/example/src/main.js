8dc7e11902fe78c61535ef4ab0364855bf942049
Tyom Semonov <inspiro@gmail.com>
2021-08-25 23:07:47 +0100

    Remove the previous version

--- packages/example/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import HeadUp from 'head-up';
-import App from './App.vue';
-
-Vue.config.productionTip = false;
-
-Vue.use(HeadUp);
-
-new Vue({ render: h => h(App) }).$mount('#app');
--- packages/head-up/src/components/Cell/__tests__/CellSettings.test.js
+++
@@ -1,7 +0,0 @@
-import { shallowMount } from '@vue/test-utils';
-import CellSettings from '../CellSettings';
-
-test('default render', () => {
-  const wrapper = shallowMount(CellSettings);
-  expect(wrapper).toMatchSnapshot();
-});
