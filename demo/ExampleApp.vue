7ab328d80b03efa1202d213aaf27880f9a141194
Tyom Semonov <inspiro@gmail.com>
2021-10-09 11:45:54 +0100

    Extract demo movies board into a separate component

--- demo/ExampleApp.vue
+++ demo/ExampleApp.vue
@@ -1,46 +1,10 @@
 <script setup>
-import { CircleGauge, VCard, VCollection } from '../src';
-const tmdbApiKey = import.meta.env.VITE_TMDB_API_KEY;
-const TMDB_IMAGE_PREFIX = 'https://image.tmdb.org/t/p/w342/';
-
-const pluralize = (val, number) => (number > 1 ? `${val}s` : val);
+import MoviesBoard from './MoviesBoard.vue';
 </script>
 
 <template>
   <HeadUp>
-    <HeadUpBoard title="Movies">
-      <HeadUpCell
-        v-slot="{ results }"
-        :http="{
-          get: 'https://api.themoviedb.org/3/discover/movie',
-          params: { api_key: tmdbApiKey },
-          refresh: '5m',
-        }"
-      >
-        <VCollection v-slot="item" :items="results">
-          <VCard
-            :title="item.original_title"
-            :image="`${TMDB_IMAGE_PREFIX}/${item.poster_path}`"
-          >
-            <CircleGauge :value="item.vote_average">
-              {{ item.vote_count.toLocaleString() }}
-              {{ pluralize('vote', item.vote_count) }}
-            </CircleGauge>
-            <p class="line-clamp-6">{{ item.overview }}</p>
-            <p class="opacity-70">
-              Released:
-              {{
-                new Date(item.release_date).toLocaleString('en-GB', {
-                  day: 'numeric',
-                  month: 'short',
-                  year: 'numeric',
-                })
-              }}
-            </p>
-          </VCard>
-        </VCollection>
-      </HeadUpCell>
-    </HeadUpBoard>
+    <MoviesBoard />
     <HeadUpBoard title="Board 2">
       <HeadUpCell title="Cell 1"> Awesome #1 </HeadUpCell>
       <HeadUpCell title="Cell 2"> Awesome #2 </HeadUpCell>
---
+++ demo/MoviesBoard.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { CircleGauge, VCard, VCollection } from '../src';
+const tmdbApiKey = import.meta.env.VITE_TMDB_API_KEY;
+
+const pluralize = (val, number) => (number > 1 ? `${val}s` : val);
+const getPosterImage = (path) => `https://image.tmdb.org/t/p/w342/${path}`;
+const formatDate = (date) =>
+  new Date(date).toLocaleString('en-GB', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  });
+</script>
+
+<template>
+  <HeadUpBoard title="Movies">
+    <HeadUpCell
+      v-slot="{ results }"
+      :http="{
+        get: 'https://api.themoviedb.org/3/discover/movie',
+        params: { api_key: tmdbApiKey },
+        refresh: '5m',
+      }"
+    >
+      <VCollection v-slot="item" :items="results">
+        <VCard
+          :title="item.original_title"
+          :image="getPosterImage(item.poster_path)"
+        >
+          <CircleGauge :value="item.vote_average">
+            {{ item.vote_count.toLocaleString() }}
+            {{ pluralize('vote', item.vote_count) }}
+          </CircleGauge>
+          <p class="line-clamp-6">{{ item.overview }}</p>
+          <p class="opacity-70">
+            Released: {{ formatDate(item.release_date) }}
+          </p>
+        </VCard>
+      </VCollection>
+    </HeadUpCell>
+  </HeadUpBoard>
+</template>
